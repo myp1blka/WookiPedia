cd8c1a5aa939dccfc2a72eb7ac87535f46b36aad
Vit Mur <myp1blka@gmail.com>
2022-07-09 19:58:41 +0300

    bug fix and comment

--- js/main.js
+++ js/main.js
@@ -11,34 +11,40 @@ let chooseYouPlanet =  parseInt( elChoosePlanet.value );
 
 // ##############################################################
 const DrawPlanets = (result) => {
-    let planetParams = "";
-    for (var key in result.data) {
-        planetParams += `<p>${key}:   ${result.data[key]} </p>`;
-        //console.log("key: " + key + " значение: " + result.data[key] );
+    let planetParams = ""; // очистити накопичувальну HTML змінну
+    for (var key in result.data) { // перелічити всі параметри в об'єкті та їх значення
+        planetParams += `<p>${key}:   ${result.data[key]} </p>`; // форматуємо виводимі дані
     }
-    elField2.innerHTML = "";
-    elField2.insertAdjacentHTML('beforeend', planetParams);
+    elField2.insertAdjacentHTML('beforeend', planetParams); // виводимо всі дані накопичені у змінній
 }
 
 // ##############################################################
+// Отримати планету по номеру
 const GetOverHere = async (chooseYouPlanet) => {
-    if (chooseYouPlanet != "" && chooseYouPlanet > 0){
-    elField2.innerHTML = "";
-    elField2.innerHTML = ' Loading...  <img src="./img/loading.gif"> ';
-    const resultPlanetObj = await axios.get('https://swapi.dev/api/planets/' + chooseYouPlanet + '/');
-    //console.log (resultPlanetObj.data);
-    DrawPlanets(resultPlanetObj);
+    if (chooseYouPlanet != "" && chooseYouPlanet > 0) { // Перевірка чи число ми отримали від користувача
+        // очистити блок виводу
+        elField2.innerHTML = ""; // очистити блок виводу
+        // показати анімацію завантаження
+        elField2.innerHTML = ' Loading...  <img src="./img/loading.gif" alt="Loading..."> ';
+        // отримати дані від віддаленого сервера
+        const resultPlanetObj = await axios.get('https://swapi.dev/api/planets/' + chooseYouPlanet + '/');
+        // відмалювати отримані дані в HTML (передаємо об'єкт)
+        DrawPlanets(resultPlanetObj);
     }
     else {
+        // якщо користувач ввів літеру або нічого не ввів
         elField2.innerHTML = " Пленету не обрано ";
     }
 }
 
 // ##############################################################
+// Взаємодія з користувачем
+
+// якщо змінюється випадаючий список
 elChoosePlanet.addEventListener('change', () => {GetOverHere(parseInt( elChoosePlanet.value ))});
-
+// якщо клік на кнопці
 elGetPlanet.addEventListener('click', () => {GetOverHere(parseInt( elNumberPlanet.value ))});
-
+// якщо натисли на клавіатурі клавішу ентер
 document.addEventListener("keydown", (e) => {
     if (e.code === "Enter")         { GetOverHere(parseInt( elNumberPlanet.value )) };
 });
