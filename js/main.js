b2be34530e209f9eb34fb2fb7d04d233e9f818a4
Vit Mur <myp1blka@gmail.com>
2022-07-09 19:28:28 +0300

    it is work 2

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 const elField1 = document.querySelector('.field1');
 const elField2 = document.querySelector('.field2');
 const elChoosePlanet = document.querySelector('.choosePlanet');
-
+const elNumberPlanet = document.querySelector('input[name="planetByNumber"]');
+const elGetPlanet = document.querySelector('input[name="bGetPlanetByNumber"]');
 
 let playField1 = parseInt( elField1.value );
 let playField2 =  parseInt( elField2.value );
@@ -18,15 +19,21 @@ const DrawPlanets = (result) => {
     elField2.innerHTML = "";
     elField2.insertAdjacentHTML('beforeend', planetParams);
 }
+
 // ##############################################################
-const GetOverHere = async () => {
+const GetOverHere = async (chooseYouPlanet) => {
     elField2.innerHTML = "";
     elField2.innerHTML = ' Loading...  <img src="./img/loading.gif"> ';
-    chooseYouPlanet =  parseInt( elChoosePlanet.value );
     const resultPlanetObj = await axios.get('https://swapi.dev/api/planets/' + chooseYouPlanet + '/');
     //console.log (resultPlanetObj.data);
     DrawPlanets(resultPlanetObj);
 }
 
 // ##############################################################
-elChoosePlanet.addEventListener('change', () => {GetOverHere()});
+elChoosePlanet.addEventListener('change', () => {GetOverHere(parseInt( elChoosePlanet.value ))});
+
+elGetPlanet.addEventListener('click', () => {GetOverHere(parseInt( elNumberPlanet.value ))});
+
+document.addEventListener("keydown", (e) => {
+    if (e.code === "Enter")         { GetOverHere(parseInt( elNumberPlanet.value )) };
+});
